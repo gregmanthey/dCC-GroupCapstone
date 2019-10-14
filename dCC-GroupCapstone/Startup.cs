17bd989221c275a80f4e4cde05de05c9bbe2c0d8
gregmanthey <gmanthey03@gmail.com>
2019-10-14 08:51:18 -0500

    Created Visual Studio project and ran enable-migrations in NuGet Package Manager console.

---
+++ dCC-GroupCapstone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dCC_GroupCapstone.Startup))]
+namespace dCC_GroupCapstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
