0c31a356a4d793a11bb46f5a04cb0ea406477716
cwadeking <charles.w.king.cwk@gmail.com>
2019-10-15 09:46:07 -0500

    Created basic crud ops for the vacation controller.

---
+++ dCC-GroupCapstone/Controllers/VacationController.cs
@@ -0,0 +1,99 @@
+﻿using dCC_GroupCapstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dCC_GroupCapstone.Controllers
+{
+    public class VacationController : Controller
+       
+    {
+        ApplicationDbContext context;
+
+        public VacationController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Vacation
+        public ActionResult Index()
+        {
+            return View(context.Vacations);//probably need to change this view to recently created vacation? or all vacations?
+        }
+
+        // GET: Vacation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(context.Vacations.Where(v => v.Id == id).FirstOrDefault());
+        }
+
+        // GET: Vacation/Create
+        public ActionResult Create()
+        {
+            Vacation vacation = new Vacation();
+            return View(vacation);
+        }
+
+        // POST: Vacation/Create
+        [HttpPost]
+        public ActionResult Create(Vacation vacation)
+        {
+            try
+            {
+                context.Vacations.Add(vacation);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Vacation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var vacationEdit = context.Vacations.Find(id);
+            return View(vacationEdit);
+        }
+
+        // POST: Vacation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Vacation vacation)
+        {
+            try
+            {
+                var vacationInDb = context.Vacations.Find(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Vacation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(context.Vacations.Find(id));
+        }
+
+        // POST: Vacation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Vacation vacation)
+        {
+            try
+            {
+                Vacation selectedVacation = context.Vacations.Find(id);
+                context.Vacations.Remove(selectedVacation);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
