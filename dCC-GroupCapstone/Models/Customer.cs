7866263c749be17082065cbd1160b88ca74500a8
cwadeking <charles.w.king.cwk@gmail.com>
2019-10-17 13:18:18 -0500

    Corrected display names inside models for better viewing.

--- dCC-GroupCapstone/Models/Customer.cs
+++ dCC-GroupCapstone/Models/Customer.cs
@@ -11,9 +11,13 @@ namespace dCC_GroupCapstone.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Interests")]
         public ICollection<Interest> Interests { get; set; }
+        [Display(Name = "Saved Vacations")]
         public ICollection<Vacation> SavedVacations { get; set; }
 
         [ForeignKey("User")]
--- dCC-GroupCapstone/Models/Vacation.cs
+++ dCC-GroupCapstone/Models/Vacation.cs
@@ -11,14 +11,19 @@ namespace dCC_GroupCapstone.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Name of Vacation")]
         public string VacationName { get; set; }
+        [Display(Name = "Mark as private?")]
         public bool IsPrivate { get; set; }
+        [Display(Name = "Location")]
         public string LocationQueried { get; set; }
         public int CustomerCreated { get; set; }
         public double Cost { get; set; }
         [ForeignKey("Hotel")]
+        [Display(Name = "Hotel Name")]
         public int SavedHotel { get; set; }
         public Hotel Hotel { get; set; }
+        [Display(Name = "Rating")]
         public double AverageRating { get; set; }
         public ICollection<Activity> Activities { get; set; }
     }
